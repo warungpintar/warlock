23df0e5f9e0eb24d3bf837ac55930bc47d15175b
Ade Yahya Prasetyo <ade.yahya@warungpintar.co>
2021-03-25 09:33:25 +0000

    feat: config validation

--- src/cli/runServer.ts
+++ src/cli/runServer.ts
@@ -1,7 +1,8 @@
 import * as esbuild from 'esbuild';
 import path from 'path';
+import chalk from 'chalk';
 import { pipe } from 'fp-ts/function';
-import { map, mapLeft } from 'fp-ts/Either';
+import { bimap } from 'fp-ts/Either';
 
 import { run } from '../express';
 import { getConfig } from '../config';
@@ -11,32 +12,29 @@ import { getPatResolvers } from '../utils';
 import { PATH_RESOLVER_DIR } from '../constant';
 
 const runServer = (config: string, port: number) => {
+  console.clear();
   const filePath = path.join(process.cwd(), config ?? '.warlock.yaml');
   pipe(
     getConfig(filePath),
-    mapLeft(logger.error),
-    map((c) => {
-      const pathResolvers: string[] = getPatResolvers(c);
-
-      console.time(`building ${pathResolvers.length} resolvers`);
-      pathResolvers.forEach((resolverPath) => {
-        esbuild.buildSync({
-          entryPoints: [path.join(process.cwd(), resolverPath)],
-          bundle: true,
-          minify: true,
-          platform: 'node',
-          format: 'cjs',
-          outfile: path.join(PATH_RESOLVER_DIR, resolverPath),
-        });
-      });
-      logger.info('building resolvers');
-      console.timeEnd(`building ${pathResolvers.length} resolvers`);
-
-      const server = run({ port, config: c });
-
-      // watch resolvers
-      pathResolvers.forEach((resolverPath) => {
+    bimap(
+      (e) => {
+        // watch config
         fileWatcher(() => {
+          runServer(config, port);
+        })(filePath);
+        e.forEach((errItem) => {
+          logger.error(
+            chalk.underline.red(errItem?.problem) +
+              ' | ' +
+              chalk.yellow(errItem?.reason),
+          );
+        });
+      },
+      (c) => {
+        const pathResolvers: string[] = getPatResolvers(c);
+
+        console.time(`building ${pathResolvers.length} resolvers`);
+        pathResolvers.forEach((resolverPath) => {
           esbuild.buildSync({
             entryPoints: [path.join(process.cwd(), resolverPath)],
             bundle: true,
@@ -45,18 +43,36 @@ const runServer = (config: string, port: number) => {
             format: 'cjs',
             outfile: path.join(PATH_RESOLVER_DIR, resolverPath),
           });
-          logger.info('restaring server');
-        })(path.join(process.cwd(), resolverPath));
-      });
-
-      // watch config
-      fileWatcher(() => {
-        server.close(() => {
-          runServer(config, port);
-          logger.info('restaring server');
         });
-      })(filePath);
-    }),
+        logger.info('building resolvers');
+        console.timeEnd(`building ${pathResolvers.length} resolvers`);
+
+        const server = run({ port, config: c });
+
+        // watch resolvers
+        pathResolvers.forEach((resolverPath) => {
+          fileWatcher(() => {
+            esbuild.buildSync({
+              entryPoints: [path.join(process.cwd(), resolverPath)],
+              bundle: true,
+              minify: true,
+              platform: 'node',
+              format: 'cjs',
+              outfile: path.join(PATH_RESOLVER_DIR, resolverPath),
+            });
+            logger.info('restaring server');
+          })(path.join(process.cwd(), resolverPath));
+        });
+
+        // watch config
+        fileWatcher(() => {
+          server.close(() => {
+            runServer(config, port);
+            logger.info('restaring server');
+          });
+        })(filePath);
+      },
+    ),
   );
 };
 
--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,37 @@
 import * as yaml from 'js-yaml';
-// import path from 'path';
 import * as fs from 'fs';
 import * as E from 'fp-ts/lib/Either';
 import * as IOE from 'fp-ts/lib/IOEither';
 import * as F from 'fp-ts/lib/function';
 import * as T from './types';
-/**
- * TODO : make it utils for this project
- */
+import {
+  validate,
+  validateString,
+  validateRequired,
+  validateFqdn,
+} from '@warungpintar/ninshu';
+
+const VERB_LIST = ['get', 'post', 'delete', 'patch', 'put'];
+
+const validateUrl = validate((url: string) => {
+  try {
+    // eslint-disable-next-line no-new
+    new URL(url);
+    return true;
+  } catch {
+    return false;
+  }
+});
+
+const validateEmptyQs = validate((url: URL) => {
+  return !url.search;
+});
+
+type ConfigError = {
+  problem: string;
+  reason: string;
+};
+
 const readFileSync = (file: string): IOE.IOEither<Error, string> =>
   IOE.tryCatch(() => fs.readFileSync(file, 'utf8'), E.toError);
 
@@ -16,19 +40,101 @@ const emptyConfig: T.Config = {
   graphql: undefined,
 };
 
+const returnValidObjectOrEmptyObject = (val: any): (() => T.Config) => () =>
+  typeof val === 'object' ? val : emptyConfig;
+
 const yamlLoad = (
   file: string,
   opts?: yaml.LoadOptions,
-): IOE.IOEither<Error, T.Config> =>
-  IOE.tryCatch(() => {
-    const loaded = yaml.load(file, opts);
-    switch (typeof loaded) {
-      case 'object':
-        return loaded ?? emptyConfig;
-      default:
-        return emptyConfig;
-    }
-  }, E.toError);
+): IOE.IOEither<ConfigError[], T.Config> =>
+  IOE.tryCatch(
+    F.pipe(yaml.load(file, opts), returnValidObjectOrEmptyObject),
+    (reason) => [
+      {
+        problem: file,
+        reason: String(reason),
+      },
+    ],
+  );
+
+const validateConfig = (
+  config: T.Config,
+): E.Either<ConfigError[], T.Config> => {
+  const { rest } = config;
+  const sources = rest?.sources ?? [];
+
+  const errors = sources.reduce((prev, next) => {
+    const origin = next.origin ?? '';
+    const transforms = next.transforms ?? {};
+    const _origin = origin.replace(/^(http|https):\/\//, '');
+    const concatPrev = (e: ConfigError) => {
+      prev = [...prev, e];
+    };
+
+    // validate origin
+    F.flow(
+      validateRequired("origin can't be nil"),
+      E.chain(validateString('origin should be string')),
+      E.chain(validateFqdn('invalid origin')),
+      E.mapLeft((e) => {
+        concatPrev({ problem: origin ?? 'undefined origin', reason: e });
+      }),
+    )(_origin);
+
+    // validate path
+    Object.keys(transforms).forEach((tpath) => {
+      F.flow(
+        validateRequired("path can't be nil"),
+        E.chain(validateString('path should be string')),
+        E.map((val) => origin.concat(val)),
+        E.chain(validateUrl('broken origin')),
+        E.map((val) => new URL(val)),
+        E.chain(validateEmptyQs("path can't contain query string")),
+        E.mapLeft((e) => {
+          concatPrev({ problem: tpath, reason: e });
+        }),
+      )(tpath);
+    });
+
+    Object.values(transforms).forEach((tval) => {
+      const verbs = Object.keys(tval ?? {});
+      const verbsHandler = Object.values(tval ?? {});
+
+      // validate verb
+      verbs.forEach((verb) => {
+        if (!VERB_LIST.includes(verb.toLocaleLowerCase())) {
+          concatPrev({
+            problem: verb,
+            reason: 'only accept '.concat(VERB_LIST.join(', ')),
+          });
+        }
+      });
+
+      // validate handler
+      verbsHandler.forEach((handlers: T.WarlockConfig.Field[]) => {
+        handlers.forEach((handler) => {
+          const parts = handler.field?.split('.') ?? [];
+          // @TODO: validate resolver
+
+          if (parts[0] !== 'root') {
+            concatPrev({
+              problem: handler.field ?? 'undefined field',
+              reason: 'field should always starts with root',
+            });
+          }
+        });
+      });
+    });
+
+    return prev;
+  }, [] as ConfigError[]);
+
+  if (errors.length > 0) {
+    return E.left(errors);
+  }
+
+  return E.right(config);
+};
 
 /**
  * this function return WarlockConfig
@@ -44,11 +150,11 @@ export const getConfig = (file: string) => {
   const p = F.pipe(
     file,
     readFileSync,
-    IOE.bimap(E.toError, yamlLoad),
+    IOE.bimap(() => [{ problem: file, reason: "can't read file" }], yamlLoad),
     IOE.flatten,
   );
 
-  return p();
+  return E.chain(validateConfig)(p());
 };
 
 export const getConfigWithDefault = (file: string) => {
