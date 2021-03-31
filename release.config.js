d71031667e7f35b8b71a58af7a34ffcd42e24662
Mirza Adipradhana <mirza.adipradhana@warungpintar.co>
2021-03-31 10:30:30 +0700

    test(functional): run test sequentially to avoid race condition

--- release.config.js
+++ release.config.js
@@ -5,8 +5,16 @@ module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    '@semantic-release/gitlab',
+    ["@semantic-release/gitlab", {
+      "gitlabUrl": "https://gitlab.warungpintar.co"
+    }],
     '@semantic-release/npm',
-    '@semantic-release/git'
+    [
+      "@semantic-release/git",
+      {
+        "assets": ["package.json"],
+        "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+      }
+    ]
   ]
 }
