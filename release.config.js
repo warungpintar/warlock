cf241a9877231a7dc512adb416b73f1cdd33a461
Mirza Adipradhana <mirza.adipradhana@warungpintar.co>
2021-04-12 14:05:22 +0700

    fix(changelog): generate changelog file based on semantic-release analyzer

--- release.config.js
+++ release.config.js
@@ -5,14 +5,23 @@ module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    ["@semantic-release/gitlab", {
-      "gitlabUrl": "https://gitlab.warungpintar.co"
-    }],
+    [
+      "@semantic-release/gitlab",
+      {
+        "gitlabUrl": "https://gitlab.warungpintar.co"
+      }
+    ],
+    [
+      "@semantic-release/changelog",
+      {
+        "changelogFile": "CHANGELOG.md"
+      }
+    ],
     '@semantic-release/npm',
     [
       "@semantic-release/git",
       {
-        "assets": ["package.json"],
+        "assets": ["package.json","CHANGELOG.md"],
         "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
       }
     ]
